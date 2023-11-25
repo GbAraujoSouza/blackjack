fe9418f5cfe5ea274184b71157cd97d11e2a9569
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2023-11-25 20:39:24 -0300

    function to return a random card with the correct probabilities

--- main.c
+++ main.c
@@ -8,11 +8,39 @@ typedef struct {
     listaDesCartas cartas;
 } jogador;
 
+int cartaAleatoria() {
+    // Gera um numero aleatório a partir de um vetor de cartas 
+    // O elemento 'D' representa o numero 10
+    int cartas[] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'D', 'J', 'Q', 'K' };
+
+    int randomIndex = rand() % 13;
+    int randomCard = cartas[randomIndex];
+
+    // os números retornados sao contados a partir da tabela ascii
+    switch (randomCard)
+    {
+    case 74:  //J
+    case 75:  //K
+    case 81:  //Q
+    case 68:  //D
+        return 10;
+        break;
+
+    case 65:  //A
+        return 11;
+        break;
+    
+    default:  //2-9
+        return randomCard - 48;
+        break;
+    }
+}
+
 int main() {
 
     // Gerador de numero aleatório
     time_t t1;
-    srand ( (unsigned) time (&t1));
+    srand ((unsigned) time (&t1));
 
     int qntJogadores;
     printf("Numero de Jogadores ");
@@ -25,8 +53,8 @@ int main() {
     }
 
     for (int i = 0; i < qntJogadores; i++) {
-        append(&jogadores[i].cartas, rand() % 11 + 1);
-        append(&jogadores[i].cartas, rand() % 11 + 1);
+        append(&jogadores[i].cartas, cartaAleatoria());
+        append(&jogadores[i].cartas, cartaAleatoria());
     }
 
     for (int i = 0, j = 1; i < qntJogadores; i++, j++) {
